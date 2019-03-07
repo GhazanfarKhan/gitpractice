6218585fd92a6148ff5de2918d0f1626015baf3c
GhazanfarKhan <ghazanfarkhan@outlook.com>
2019-03-07 13:01:04 +0500

    merged

--- subract.js
+++ subract.js
@@ -1,5 +1,6 @@
 // subtract.js [dev/commit1]
 // return subtraction of two numbers
-function subtract(a, b, bMINUSa) {
-    return (bMINUSa) ? b - a : a - b;
+function subtract(a, b) {
+    return a - b;
+    //test
 };
